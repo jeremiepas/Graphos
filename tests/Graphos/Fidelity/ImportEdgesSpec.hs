4739559459502dcf3428b12eecdcb042ff943c09
jeremie <jeremie@montech.nixos.blabla>
2026-08-26 22:43:45 +0200

    fix: repair case-alternative indentation parse error in test resolveDots
    Co-Authored-By: Paperclip <noreply@paperclip.ing>

--- tests/Graphos/Fidelity/ImportEdgesSpec.hs
+++ tests/Graphos/Fidelity/ImportEdgesSpec.hs
@@ -96,7 +96,7 @@ resolveDots p = T.unpack (T.intercalate "/" (go (T.splitOn "/" (T.pack p)) []))
       | x == ".."  = case acc of
           [] -> go rest acc
           ("" : _) -> go rest acc
-           (_ : restAcc) -> go rest restAcc
+          (_ : restAcc) -> go rest restAcc
       | otherwise  = go rest (x : acc)
 
 scanGroundTruth :: FilePath -> IO (Set.Set (FilePath, FilePath))
