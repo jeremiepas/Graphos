c893fab3834c5fee0ca000641c3485fffea7a068
Jeremie <jeremie@we-framework.com>
2026-08-26 16:10:41 +0200

    fix: correct indentation in resolveDots to fix build failure

--- tests/Graphos/Fidelity/ImportEdgesSpec.hs
+++ tests/Graphos/Fidelity/ImportEdgesSpec.hs
@@ -94,8 +94,8 @@ resolveDots p = T.unpack (T.intercalate "/" (go (T.splitOn "/" (T.pack p)) []))
     go (x : rest) acc
       | x == "."   = go rest acc
       | x == ".."  = case acc of
-          [] -> go rest acc
-          ("" : _) -> go rest acc
+           [] -> go rest acc
+           ("" : _) -> go rest acc
            (_ : restAcc) -> go rest restAcc
       | otherwise  = go rest (x : acc)
 
