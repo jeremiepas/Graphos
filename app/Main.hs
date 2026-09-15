96b1e9f2a65782a13d8a43157bf57161acf51d91
head-of-dev (paperclip) <noreply@paperclip.ing>
2026-09-15 03:25:55 +0200

    AVI-575: fix graphos CLI query build (import scoped scored query + toCachedFGL arg)

--- app/Main.hs
+++ app/Main.hs
@@ -31,7 +31,7 @@ import Graphos.UseCase.Pipeline (runPipeline, runClusterOnlyPipeline, runIncreme
 import Graphos.Infrastructure.Wiring (productionAppEnv)
 import Graphos.UseCase.AppEnv (AppEnv(..))
 import Graphos.UseCase.Load (loadGraphFromFile, loadGraphFromFileStrict, LoadResult(..), validateGraphFile, corruptGraphMessage)
-import Graphos.UseCase.Query (queryGraphWithIndexScored, pathQueryWithIndex, explainNodeWithIndex, symbolLookup, neighborhoodExpansion, resolveNodeArg, NodeResolution(..), QueryResponse(..))
+import Graphos.UseCase.Query (queryGraphWithIndexScoredScoped, pathQueryWithIndex, explainNodeWithIndex, symbolLookup, neighborhoodExpansion, resolveNodeArg, NodeResolution(..), QueryResponse(..))
 import Graphos.Domain.Query.Cypher.Parser (parseStatement)
 import Graphos.Domain.Query.Cypher.AST (CypherStatement(..))
 import Graphos.Domain.Query.Cypher.Eval (evaluateStatement)
@@ -41,7 +41,7 @@ import Graphos.Domain.Community (computeCompositions)
 import Graphos.UseCase.Merge (mergeGraphsAndAnalyze, MergeResult(..))
 import qualified Graphos.UseCase.Merge as Merge (mrGraph)
 import Graphos.Domain.Graph (Graph, gNodes, gEdges, gAdjFwd, gAdjBack, neighbors, degree)
-import Graphos.Domain.Graph.Analysis (articulationPoints)
+import Graphos.Domain.Graph.Analysis (articulationPoints, toCachedFGL)
 import Graphos.Domain.Graph.Index (communityOfNode)
 import Graphos.UseCase.Query.Refine (RefineConfig(..), refineResponse)
 import Graphos.UseCase.Query.Render (CommonQueryOpts(..), renderQueryResponseText, renderQueryResponseJSON, renderSymbolResultText, renderSymbolResultJSON, renderNeighborsResultText, renderNeighborsResultJSON, renderPathResultJSON, renderExplainResultJSON, renderAmbiguousText, renderAmbiguousJSON, renderNotFoundText, renderNotFoundJSON, renderMutationResultText, renderMutationResultJSON)
@@ -297,7 +297,7 @@ main = do
         Right loaded -> do
            let g = lrGraph loaded
                idx = lrIndex loaded
-               scoredResp0 = queryGraphWithIndexScoredScoped g idx question mode budget (cqoPath qopts)
+               scoredResp0 = queryGraphWithIndexScoredScoped g idx (toCachedFGL g) question mode budget (cqoPath qopts)
                scoredResp = case cqoMaxNodes qopts of
                  Just n | n > 0 -> scoredResp0 { qrespNodes = take n (qrespNodes scoredResp0) }
                  _ -> scoredResp0
